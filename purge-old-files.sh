027cad45dd99d8212c9b79b0cd413a328bd49980
Zhichun Wu <zhicwu@gmail.com>
2017-06-01 14:18:49 +0800

    Purge logs under tomcat/logs directory

--- purge-old-files.sh
+++ purge-old-files.sh
@@ -6,7 +6,7 @@ log() {
 }
 
 log "INFO" "Removing temporary files that have not been modified within one day under $BISERVER_HOME/tomcat/temp directory..."
-find $BISERVER_HOME/tomcat/temp/* -maxdepth 0 -name "*.*" -mtime +1 | xargs rm -f
-log "INFO" "Removing log files created 21 days ago under /tmp/kettle directory..."
-find /tmp/kettle -name "*.log" -mtime +21 | xargs rm -f
+find $BISERVER_HOME/tomcat/temp/* -maxdepth 0 -type f -name "*.*" -mtime +1 | xargs rm -f
+log "INFO" "Removing log files created 21 days ago under /tmp/kettle and $BISERVER_HOME/tomcat/logs directories..."
+find /tmp/kettle $BISERVER_HOME/tomcat/logs -type f -iname "*.log" -o -iname "*.txt" -mtime +21 | xargs rm -f
 log "INFO" "Done"
